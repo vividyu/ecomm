dd793b2ada1929c77fa88a16b191a1a20647ca9f
Samuel <sixgod666@gmail.com>
2023-06-13 14:36:15 -0500

    22

--- src/Ecomm.js
+++ src/Ecomm.js
@@ -10,10 +10,12 @@ function Ecomm() {
     }
 
     return (
-        <div>
-            <input placeholder="Search..."></input>
-            <Link className="login-link" to="/login">Login</Link>
-            <Link className="bag-link" to="/bag">Bag</Link>
+        <div className="main-container">
+            <nav>
+                <input className="search-bar" placeholder="Search..."></input>
+                <Link className="login-link" to="/login">Login</Link>
+                <Link className="bag-link" to="/bag">Bag</Link>
+            </nav>
             <div className="item-container">
                 {arr.map(i => <Item title="basketball" price="2.5" />)}
             </div>
